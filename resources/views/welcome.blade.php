9ad8e58543ae8219d3ddded143bb2bf3395dc415
Tyler <yumenohik@gmail.com>
2023-10-25 17:35:31 +0900

    Build the Keyboard

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -18,9 +18,14 @@
 <body>
     <main x-data="game"
           @keyup.window="onKeyPress($event.key)">
-        <h1 aria-label="TryCat">
-            <img src="/images/trycat-logo.svg" alt="">
-        </h1>
+        <header>
+            <h1 aria-label="TryCat">
+                <img src="/images/trycat-logo.svg" alt="">
+            </h1>
+
+            <output x-text="message"></output>
+        </header>
+
         <div id="game">
             <template x-for="(row, index) in board">
                 <div class="row" :class="{'current' : currentRowIndex === index, 'invalid' : currentRowIndex === index && errors}">
@@ -31,8 +36,15 @@
             </template>
         </div>
 
-        <output x-text="message"></output>
+        <div id="keyboard" @click.stop="$event.target.matches('button') && onKeyPress($event.target.textContent)">
+            <template x-for="row in letters">
+                <div class="row">
+                    <template x-for="key in row">
+                        <button type="button" x-text="key"></button>
+                    </template>
+                </div>
+            </template>
+        </div>
     </main>
-
 </body>
 </html>
